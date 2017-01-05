e249b25960f2d20b921a282a8de7df0374eb3bca
Robert Novotny <robert.novotny@dcoresystems.com>
2017-01-05 18:48:48 +0100

    Aggregate all frequencies from actors

--- src/main/java/sk/upjs/ics/novotnyr/akka/MasterActor.java
+++ src/main/java/sk/upjs/ics/novotnyr/akka/MasterActor.java
@@ -7,6 +7,7 @@ import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import akka.routing.RoundRobinPool;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MasterActor extends UntypedActor {
@@ -16,13 +17,17 @@ public class MasterActor extends UntypedActor {
 			                            .actorOf(Props.create(SentenceCountActor.class)
                                                       .withRouter(new RoundRobinPool(3)));
 
+    private Map<String, Integer> allFrequencies = new HashMap<>();
+
     @Override
 	public void onReceive(Object message) throws Exception {
 	    if (message instanceof String) {
 	        sentenceCounter.tell(message, getSelf());
         } else if (message instanceof Map) {
-	        Map frequencies = (Map) message;
-	        logger.info(frequencies.toString());
+            @SuppressWarnings("unchecked")
+	        Map<String, Integer> frequencies = (Map<String, Integer>) message;
+
+            allFrequencies = MapUtils.aggregate(frequencies, allFrequencies);
         } else {
 	        unhandled(message);
         }
