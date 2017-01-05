0a9d1ee8d155e5dbe86f069c8fbe30e3c1d25630
Robert Novotny <robert.novotny@dcoresystems.com>
2017-01-05 18:43:23 +0100

    Establish three actors behind a round-robin router

--- src/main/java/sk/upjs/ics/novotnyr/akka/MasterActor.java
+++ src/main/java/sk/upjs/ics/novotnyr/akka/MasterActor.java
@@ -5,6 +5,7 @@ import akka.actor.Props;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
+import akka.routing.RoundRobinPool;
 
 import java.util.Map;
 
@@ -12,9 +13,10 @@ public class MasterActor extends UntypedActor {
     private LoggingAdapter logger = Logging.getLogger(getContext().system(), this);
 
 	private ActorRef sentenceCounter = getContext()
-			                            .actorOf(Props.create(SentenceCountActor.class));
+			                            .actorOf(Props.create(SentenceCountActor.class)
+                                                      .withRouter(new RoundRobinPool(3)));
 
-	@Override
+    @Override
 	public void onReceive(Object message) throws Exception {
 	    if (message instanceof String) {
 	        sentenceCounter.tell(message, getSelf());
