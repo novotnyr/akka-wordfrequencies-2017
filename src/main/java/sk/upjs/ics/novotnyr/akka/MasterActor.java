837cdcf3baf1c9365ed2be55412416f29f59bb08
Robert Novotny <novotnr0@gmail.com>
2018-12-18 07:00:05 +0100

    Add message routing from Master->SentenceCounter

--- src/main/java/sk/upjs/ics/novotnyr/akka/MasterActor.java
+++ src/main/java/sk/upjs/ics/novotnyr/akka/MasterActor.java
@@ -1,5 +1,7 @@
 package sk.upjs.ics.novotnyr.akka;
 
+import akka.actor.ActorRef;
+import akka.actor.Props;
 import akka.actor.UntypedActor;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
@@ -7,9 +9,15 @@ import akka.event.LoggingAdapter;
 public class MasterActor extends UntypedActor {
     private LoggingAdapter logger = Logging.getLogger(getContext().system(), this);
 
+	private ActorRef sentenceCounter = getContext().actorOf(Props.create(SentenceCountActor.class));
+
 	@Override
 	public void onReceive(Object message) throws Exception {
-		unhandled(message);
+		if (message instanceof String) {
+			sentenceCounter.tell(message, getSelf());
+		} else {
+			unhandled(message);
+		}
 	}
 
 }
